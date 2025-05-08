77d27fabe9dfa2e529685847c391a30c520bf401
Pavlo Romanyuk <promanyuk@switchautomation.com>
2025-05-08 13:38:24 +0300

    chore: added grove human presence sensor  component

---
+++ esphome/components/grove_human_presence/grove_human_presence.cpp
@@ -0,0 +1,16 @@
+#include "esphome/core/log.h"
+#include "grove_human_presence.h"
+
+namespace esphome {
+namespace grove_human_presence {
+
+static const char *TAG = "grove_human_presence.sensor";
+
+void GroveHumanPresenceSensor::setup() {}
+
+void GroveHumanPresenceSensor::update() {}
+
+void GroveHumanPresenceSensor::dump_config() {}
+
+}  // namespace grove_human_presence
+}  // namespace esphome
