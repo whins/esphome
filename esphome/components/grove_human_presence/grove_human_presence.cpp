29dd1404d8644a4b65be0ffa7c4d2865d91fba37
Pavlo Romanyuk <promanyuk@switchautomation.com>
2025-05-08 14:07:52 +0300

    chore: updates

--- esphome/components/grove_human_presence/grove_human_presence.cpp
+++ esphome/components/grove_human_presence/grove_human_presence.cpp
@@ -8,7 +8,9 @@ static const char *TAG = "grove_human_presence.sensor";
 
 void GroveHumanPresenceSensor::setup() {}
 
-void GroveHumanPresenceSensor::update() {}
+float GroveHumanPresenceSensor::get_sensor_value() { return random_float() * 50; }
+
+void GroveHumanPresenceSensor::update() { this->publish_state(get_sensor_value()); }
 
 void GroveHumanPresenceSensor::dump_config() {}
 
