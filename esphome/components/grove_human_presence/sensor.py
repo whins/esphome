b24f14830f9db46ebb3aff2b31d0c3f32d16b75b
Pavlo Romanyuk <whinsler@gmail.com>
2025-05-14 00:52:49 +0300

    feat: add Grove Human Presence Sensor implementation with header and source files

--- esphome/components/grove_human_presence/sensor.py
+++ esphome/components/grove_human_presence/sensor.py
@@ -122,7 +122,6 @@ CONFIG_SCHEMA = (
     .extend(i2c.i2c_device_schema(CONF_I2C_ADDR))
 )
 
-
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     yield cg.register_component(var, config)
