cbf6dbf0a8c24fdfe86199d9cf72b15ff579c9ad
Pavlo Romanyuk <whinsler@gmail.com>
2025-05-14 15:41:26 +0300

    refactor: comment out error handling in GroveHumanPresenceSensor update method

--- esphome/components/grove_human_presence/grove_human_presence.cpp
+++ esphome/components/grove_human_presence/grove_human_presence.cpp
@@ -95,11 +95,11 @@ void GroveHumanPresenceSensor::update() {
   ESP_LOGD(TAG, "Read REG_ST1 data:  %0x", data);
 
   // if (!dataReady()) {
-  if ((data & 0x01) != 0x01) {
-    ESP_LOGW(TAG, "Reading Grove Gas Sensor data failed!");
-    this->status_set_warning();
-    return;
-  }
+  // if ((data & 0x01) != 0x01) {
+  //   ESP_LOGW(TAG, "Reading Grove Gas Sensor data failed!");
+  //   this->status_set_warning();
+  //   return;
+  // }
 
   ESP_LOGI(TAG, "Read data");
 
