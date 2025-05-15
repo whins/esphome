73bccebea16f8732a360cf06c51945d48bf4c175
Pavlo Romanyuk <whinsler@gmail.com>
2025-05-15 23:03:03 +0300

    refactor: update sensitivity values for presence and movement in Grove Human Presence Sensor

--- esphome/components/grove_human_presence/grove_human_presence.cpp
+++ esphome/components/grove_human_presence/grove_human_presence.cpp
@@ -9,8 +9,8 @@ namespace grove_human_presence {
 #define TIMEOUT_MS 1000
 
 void GroveHumanPresenceSensor::setup() {
-  sensitivity_presence = 6.0;
-  sensitivity_movement = 10.0;
+  sensitivity_presence = 60.0;
+  sensitivity_movement = 100.0;
   detect_interval = 30;
   m_last_time = millis();
   m_movement = MOVEMENT_NONE;
